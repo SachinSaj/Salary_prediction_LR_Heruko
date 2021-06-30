9bdd8d819eacd7a1ba75ac812a443dff483b564a
Sachin Saj T K <45316683+SachinSaj@users.noreply.github.com>
2021-06-30 14:17:08 +0530

    First_Upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+from flask import Flask, render_template, request, redirect, url_for
+
+filename = 'static/model/SalPred_model.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    if request.method == "POST":
+        exp = request.form['experience']
+        exp_ = np.array(exp)
+        exp_ = exp_.reshape(-1,1)
+        salary = model.predict(exp_)
+        return render_template('index.html', pred=int(salary[0]), experience = exp)
+        
+if __name__ == '__main__':
+    app.run()
